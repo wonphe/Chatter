00cdae9c269e9bab358f15c56c96cd86641a31df
wonphe <wonphe@gmail.com>
2018-07-02 16:24:07 +0800

    init

---
+++ Chatter/FrmMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace 碎碎念
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.sslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(287, 133);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "朗读";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.BackColor = System.Drawing.SystemColors.Window;
+            this.txtText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtText.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtText.Location = new System.Drawing.Point(0, 0);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.ReadOnly = true;
+            this.txtText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtText.Size = new System.Drawing.Size(384, 181);
+            this.txtText.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsProgress,
+            this.sslStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 159);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsProgress
+            // 
+            this.tsProgress.Name = "tsProgress";
+            this.tsProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // sslStatus
+            // 
+            this.sslStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.sslStatus.Name = "sslStatus";
+            this.sslStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 181);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "碎碎念";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sslStatus;
+        private System.Windows.Forms.ToolStripProgressBar tsProgress;
+    }
+}
---
+++ Chatter/FrmMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Web;
+using System.Configuration;
+
+namespace 碎碎念
+{
+    public partial class FrmMain : Form
+    {
+        string[] _lines;
+        private MP3Player _player;
+        private string _path = "./temp.mp3";
+        private string _indexTempFile = "temp.ssn";
+        private int _index;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            string fileName = ConfigurationManager.AppSettings["fileName"].Trim();
+            _lines = File.ReadAllLines(fileName);
+            var temp = File.ReadAllLines(_indexTempFile)[0];
+            _index = string.IsNullOrEmpty(temp) ? 0 : Convert.ToInt32(temp);
+            txtText.Text = _lines[_index];
+            tsProgress.Maximum = _lines.Length;
+            tsProgress.Value = _index;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            while (this.Visible)
+            {
+                Read(_index);
+                File.WriteAllText(_indexTempFile, Convert.ToString(++_index));
+            }
+
+        }
+
+        private void Read(int index)
+        {
+            string text = _lines[index];
+            if (string.IsNullOrEmpty(text) || text == "\0") return;
+
+            txtText.Text = text;
+
+            // 将中文转换为URL，格式为UTF-8
+            text = HttpUtility.UrlEncode(text, System.Text.Encoding.GetEncoding("UTF-8"));
+
+            btnStart.Enabled = false;
+            sslStatus.Text = $"正在播放: {_index} / {_lines.Length}";
+
+            // string API_KEY = ConfigurationManager.AppSettings["API_KEY"].Trim();
+            // string SECRET_KEY = ConfigurationManager.AppSettings["SECRET_KEY"].Trim();
+            // token获得方式： https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id={API_KEY}&client_secret={SECRET_KEY}
+            string token = ConfigurationManager.AppSettings["token"].Trim();
+            string vol = ConfigurationManager.AppSettings["vol"].Trim();    // 音量，取值0-15，默认为5中音量
+            string per = ConfigurationManager.AppSettings["per"].Trim();    // 发音人选择, 0为女声，1为男声，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女
+            string spd = ConfigurationManager.AppSettings["spd"].Trim();    // 语速，取值0-9，默认为5中语速
+            string pit = ConfigurationManager.AppSettings["pit"].Trim();    // 音调，取值0-9，默认为5中语调
+            string url = $"https://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok={token}&tex={text}&vol={vol}&per={per}&spd={spd}&pit={pit}";
+            DownLoadFiles(url, _path);
+            _player = new MP3Player();
+            _player.FileName = _path;
+            _player.play();
+            tsProgress.Maximum = _lines.Length;
+            tsProgress.Value = _index;
+            Delay(_player.Duration);
+            _player.StopT();
+            File.Delete(_path);
+
+            sslStatus.Text = "";
+            btnStart.Enabled = true;
+        }
+
+        /// <summary>
+        /// Http下载文件
+        /// </summary>
+        /// <param name="uri">下载地址</param>
+        /// <param name="filefullpath">存放完整路径（含文件名）</param>
+        /// <param name="size">每次多的大小</param>
+        /// <returns>下载操作是否成功</returns>
+        public static bool DownLoadFiles(string uri, string filefullpath, int size = 1024)
+        {
+            try
+            {
+                if (File.Exists(filefullpath))
+                {
+                    try
+                    {
+                        File.Delete(filefullpath);
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+                string fileDirectory = System.IO.Path.GetDirectoryName(filefullpath);
+                if (!Directory.Exists(fileDirectory))
+                {
+                    Directory.CreateDirectory(fileDirectory);
+                }
+                FileStream fs = new FileStream(filefullpath, FileMode.Create);
+                byte[] buffer = new byte[size];
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+                request.Timeout = 10000;
+                request.AddRange((int)fs.Length);
+
+                Stream ns = request.GetResponse().GetResponseStream();
+
+                long contentLength = request.GetResponse().ContentLength;
+
+                int length = ns.Read(buffer, 0, buffer.Length);
+
+                while (length > 0)
+                {
+                    fs.Write(buffer, 0, length);
+
+                    buffer = new byte[size];
+
+                    length = ns.Read(buffer, 0, buffer.Length);
+                }
+                fs.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static void Delay(int mm)
+        {
+            DateTime current = DateTime.Now;
+            while (current.AddMilliseconds(mm) > DateTime.Now)
+            {
+                Application.DoEvents();
+            }
+            return;
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_player != null)
+            {
+                _player.StopT();
+            }
+            File.Delete(_path);
+            Environment.Exit(0);
+        }
+
+        #region sdk方式
+        ///// <summary>
+        ///// 合成
+        ///// _ttsClient = new Baidu.Aip.Speech.Tts(API_KEY, SECRET_KEY);
+        ///// </summary>
+        //public void Tts()
+        //{
+        //    string text = "这个是第一个测试文本，让百度念给我听听，哈哈哈!";
+        //    // 可选参数
+        //    var option = new Dictionary<string, object>()
+        //            {
+        //                //{"tex",text},  // 合成的文本，使用UTF-8编码，请注意文本长度必须小于1024字节
+        //                { "spd", 9}, // 语速，取值0-9，默认为5中语速
+        //                //{ "pit", 5}, // 音调，取值0-9，默认为5中语调
+        //                { "vol", 7}, // 音量，取值0-15，默认为5中音量
+        //                { "per", 1}  // 发音人选择, 0为女声，1为男声，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女
+        //            };
+        //    var result = _ttsClient.Synthesis(text, option);
+
+        //    if (result.ErrorCode == 0)  // 或 result.Success
+        //    {
+        //        File.WriteAllBytes("D://百度AI语音合成测试.mp3", result.Data);
+        //    }
+        //}
+        #endregion
+
+    }
+}
